2092314ffb7e15fa91cfff52fc07babbc17e79df
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2023-12-01 19:40:39 -0500

    minor formatting

--- header.php
+++ header.php
@@ -3,17 +3,17 @@ echo '
 <header>
     <nav>
         <ul>
-            <li><a href="learner-stories.html">Learner Stories</a></li>
-            <li><a href="practice-tests.html">Driver Tests</a></li>
-            <li><a href="document-checklist.html">Document Checklist</a></li>
+            <li><a href="learner-stories.php">Learner Stories</a></li>
+            <li><a href="practice-tests.php">Driver Tests</a></li>
+            <li><a href="document-checklist.php">Document Checklist</a></li>
             <div>
                 <img width="100" src="assets/images/logo-removebg-preview.png" alt="logo">
-                <a href="home.html">
+                <a href="index.php">
                     <p>Drive Canada</p>
                 </a>
             </div>
-            <li><a href="login.html">Login</a></li>
-            <li><a href="sign-up.html">Sign Up</a></li>
+            <li><a href="login.php">Login</a></li>
+            <li><a href="sign-up.php">Sign Up</a></li>
         </ul>
     </nav>
 </header>
