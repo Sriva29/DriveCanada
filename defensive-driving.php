6705388b785ceb7dffdfcef485a8e7236a889c12
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2023-12-05 16:26:11 -0500

    Update defensive-driving.php

--- defensive-driving.php
+++ defensive-driving.php
@@ -16,7 +16,9 @@
             <p>Watch the video and click on the hazards on the screen</p>
             <button class="nav-button" id="start-btn">Start</button>    
         </div>
-        <div id="video"></div>
+        <div id="video">
+        <iframe src="https://player.vimeo.com/video/500440443?muted=1&amp;controls=0&amp;loop=0&amp;background=1&amp;app_id=122963" width="426" height="240" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" title="toronto1" data-ready="true"></iframe>
+        </div>
     <!-- <nav class="test-navigation">
         <button class="nav-button" id="back-btn" onclick="location.href='practice-tests.php'">All Tests</button>
         <button class="nav-button" id="back-btn">Back</button>
