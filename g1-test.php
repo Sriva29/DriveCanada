dcbe98af27647364e4117db65c739bc9a5cf84b8
Sriva29 <63369374+Sriva29@users.noreply.github.com>
2023-12-06 12:54:10 -0500

    Update g1-test.php
    Database connect + shuffle implementation

--- g1-test.php
+++ g1-test.php
@@ -8,15 +8,39 @@
 </head>
 <body>
 <?php include 'header.php'?>
+<?php include 'connect.php'?>
+
+<?php
+$stmt = $pdo->prepare("SELECT * FROM `g1Test` ORDER BY `questionId` ASC LIMIT 1");  
+if($stmt->execute()){
+    $QA = $stmt->fetch();
+    if($QA){
+        // need to shuffle answers
+        $options = [$QA["option1"], $QA["option2"], $QA["option3"], $QA["correctAnswer"]];
+        shuffle($options);
+    }
+    else{
+        echo "You got a problem, mate. The fetch ain't workin'";
+    }
+} else{
+    echo "Now that's interesting. . . No statement for you 🖖";
+}
+?>
+<script> let currentQuestionId = <?=$QA["questionId"]?> </script>
+
+
 <main class="testpage-container">
     <div class="question-container">
         <div class="question-number">currentquestion#/total#ofquestions</div>
-        <div class="question">question</div>
+        <div class="question"><?=htmlspecialchars($QA["questions"])?></div>
         <div class="answers">
-            <button class="answer">answer1</button>
+            <?php foreach($options as $option):?>
+                <button class="answer"><?= htmlspecialchars($option)?></button>
+            <?php endforeach;?>
+            <!-- <button class="answer">$QA[""]</button>
             <button class="answer">answer2</button>
             <button class="answer">answer3</button>
-            <button class="answer">answer4</button>
+            <button class="answer">answer4</button> -->
         </div>
     <nav class="test-navigation">
         <button class="nav-button" id="back-btn" onclick="location.href='practice-tests.php'">All Tests</button>
